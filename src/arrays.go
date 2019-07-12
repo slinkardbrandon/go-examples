57ba3ffc69d40c5be8599772873f8d243c164979
Brandon Slinkard <slinkardbrandon@gmail.com>
2019-07-11 20:55:54 -0500

    Herpderp, shoulda check if it does really compile.

--- src/arrays.go
+++ src/arrays.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Array example: ")
+
+	// This declares an array with a length of exactly 1 item
+	var a [1]int
+	// Set that one item (index of 0, woot) to 3.
+	a[0] = 3
+
+	fmt.Println(a) // Should see the output [3]
+
 }
