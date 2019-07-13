23a2e154ae735ed82fb95145ef42028df9ad8295
Brandon Slinkard <slinkardbrandon@gmail.com>
2019-07-12 20:00:52 -0500

    Array initialization

--- src/arrays.go
+++ src/arrays.go
@@ -10,6 +10,9 @@ func main() {
 	// Set that one item (index of 0, woot) to 3.
 	a[0] = 3
 
-	fmt.Println(a) // Should see the output [3]
+	fmt.Println(a)                           // Should see the output [3]
+	fmt.Printf("Array length: %d\n", len(a)) // Output: "Array length: 1"
 
+	b := [5]int{1, 2, 3, 4, 5} // Initialize and set values to array in one line
+	fmt.Println(b)             // Output: [1 2 3 4 5]
 }
